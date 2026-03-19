e648d0619d10b7085e6d28dcecd5dc6bc5aaf5c6
David Estrada <david-a.m.p.9@hotmail.com>
2026-03-19 14:15:59 -0500

    Creacion del proyecto y Login

---
+++ domain/entities/user.py
@@ -0,0 +1,7 @@
+class User:
+    def __init__(self,id,username,password,role,is_active =True):
+        self.id = id
+        self.username = username
+        self.password = password
+        self.role = role
+        self.is_active = is_active
